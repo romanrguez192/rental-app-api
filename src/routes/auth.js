c55516b60cf85e38075ffa5a83186fdeb5566480
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-09-17 22:34:18 -0400

    Auth working

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const customers = require("./customers");
 const movies = require("./movies");
 // const rentals = require("./rentals");
 const studios = require("./studios");
+const auth = require("./auth");
 const router = express.Router();
 
 router.use("/genres", genres);
@@ -13,5 +14,6 @@ router.use("/customers", customers);
 router.use("/movies", movies);
 // router.use("/rentals", rentals);
 router.use("/studios", studios);
+router.use("/auth", auth);
 
 module.exports = router;
