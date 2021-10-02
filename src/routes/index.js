a528b72e3508ca50c15c3fbf68c77604093bebdb
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-10-02 00:25:55 -0400

    Return a movie

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const movies = require("./movies");
 const rentals = require("./rentals");
 const studios = require("./studios");
 const auth = require("./auth");
+const returns = require("./returns");
 const router = express.Router();
 
 router.use("/genres", genres);
@@ -14,6 +15,7 @@ router.use("/customers", customers);
 router.use("/movies", movies);
 router.use("/rentals", rentals);
 router.use("/studios", studios);
+router.use("/returns", returns);
 router.use("/auth", auth);
 
 module.exports = router;
---
+++ src/routes/returns.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Rental } = require("../models/Rental");
+const { Movie } = require("../models/Movie");
+const find = require("../middlewares/find");
+const validateObjectId = require("../middlewares/validateObjectId");
+const auth = require("../middlewares/auth");
+const isCustomer = require("../middlewares/isCustomer");
+const validate = require("../middlewares/validate");
+
+const validateRental = validate("rental");
+const findRental = find("rental");
+const validateId = validateObjectId("rental");
+const router = express.Router();
+
+// Return a movie
+// TODO: Validate ownership
+router.post("/:id", auth, isCustomer, validateId, findRental, async (req, res) => {
+  const rental = req.rental;
+  rental.returnDate = new Date();
+  await rental.save();
+
+  await Movie.updateOne({ _id: rental.movie }, { $inc: { numberInStock: -1 } });
+
+  res.json(rental);
+});
+
+module.exports = router;
